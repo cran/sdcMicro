f1f4dba06372d82ec456582936730f74c6d11532
Matthias Templ <Matthias.Templ@statistik.gv.at>
2007-09-05 00:00:00 +0000

    version 2.0.4

--- R/globalRecode.R
+++ R/globalRecode.R
@@ -1,6 +1,12 @@
 `globalRecode` <-
 function(x, breaks, labels){
-  gr <- cut(x, breaks=breaks, labels=labels)
+  if( class(x) == "factor" ){
+    x <- as.numeric(as.character(x))
+    gr <- cut(x, breaks=breaks, labels=labels)
+  } else{
+    gr <- cut(x, breaks=breaks, labels=labels)
+    gr <- as.numeric(as.character(gr))
+  }    
   invisible(gr)
 }
 
--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,15 @@
 .First.lib <- function(lib,pkg)
 {
   library.dynam("sdcMicro",pkg,lib)
-  cat("\n sdcMicro version 2.0.3 is loaded \n\n")
+  cat("\n sdcMicro version 2.0.4 is loaded \n\n")
+  cat("--------\n")
+  cat("If you are experiencing any problems with the package \n")
+  cat("please, send a mail to \n\n")
+  cat("       templ@statistik.tuwien.ac.at \n\n")
+  cat("It would be also interesting for me to get in contact \n") 
+  cat("with any user of the package \n")
+  cat("--------\n\n")
 }
+
 
 

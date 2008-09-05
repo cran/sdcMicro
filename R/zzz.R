e36d51deda58e2241cdc376d7777d78437f94efa
Matthias Templ <Matthias.Templ@statistik.gv.at>
2008-09-05 00:00:00 +0000

    version 2.5.5

--- R/zzz.R
+++ R/zzz.R
@@ -2,16 +2,7 @@
 {
   library.dynam("sdcMicro",pkg,lib)
   cat("\n sdcMicro version 2.5.0 has been loaded \n\n")
-  #cat("--------\n")
-  #cat("Dear Ondrej Vozar, This is a new version of the package for you which \n")
-  #cat("is not available on CRAN till now.\n")
-  #cat("If you are experiencing any problems with the package \n")
-  #cat("please, send a mail to \n\n")
-  #cat("       templ@tuwien.ac.at \n\n")
-  #cat("Have a nice day. \n")
-  #cat("It would be also interesting for me to get in contact \n") 
-  #cat("with any user of the package \n")
-  #cat("\nfix me: help files anpassen\n")
+
   cat("--------\n\n")
 }
 
